aaaf96a14e2858be19f1125d82bcfd4ac808ecd1
nthun <thomassocska@yahoo.com>
2023-11-04 08:21:29 +0100

    Some EDA

--- R/prototype.R
+++ R/prototype.R
@@ -6,7 +6,9 @@ library(tidymodels)
 
 
 # Download and unzip file ------------------------------------------------------
-googledrive::drive_download("https://drive.google.com/file/d/17-k5lBWsBudijdfAJ6oJxwVXMd_EzVlL/view?usp=drive_link", path = "data/dr2023.zip")
+# Only run this the first time
+dir.create("data/")
+googledrive::drive_download("https://drive.google.com/file/d/17-k5lBWsBudijdfAJ6oJxwVXMd_EzVlL/view?usp=drive_link", path = "data/dr2023.zip", overwrite = FALSE)
 
 # Password to unzip: data23
 system(paste("unzip -P", "data23", "data/dr2023.zip", "-d data"))
@@ -22,3 +24,6 @@ glimpse(loan_raw)
 glimpse(submission_example)
 
 count(loan_raw, TARGET_EVENT)
+n_distinct(loan_raw$BORROWER_ID)
+n_distinct(loan_raw$CONTRACT_ID)
+
